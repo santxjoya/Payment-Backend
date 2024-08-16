cf1df14ac0548afda4d49a59109f1ed2bb0a8e39
ZzxKidxzZ <mrjuans52@gmail.com>
2024-08-16 07:06:06 -0500

    Autentificacion

---
+++ routes/Auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const AuthController = require('../controllers/AuthController');
+
+const {
+    postRegister,
+    getRegister,
+    postLogin,
+    getLogin
+} = require('../controllers/AuthController');
+const router = express.Router();
+
+router.post('/register', postRegister);
+router.get('/register',  getRegister);
+
+router.post('/login', postLogin);
+router.get('/login', getLogin);
+module.exports = router;
