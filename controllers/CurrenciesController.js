191e1eb9548e0ae5034dba792e6f531922e71fdb
LinaCruzM <109240037+LinaCruzM@users.noreply.github.com>
2024-09-27 01:33:18 -0500

    cambio max currencies

--- controllers/CurrenciesController.js
+++ controllers/CurrenciesController.js
@@ -39,34 +39,18 @@ const getCurrenciesById = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-<<<<<<< HEAD
 };
 
 const updateCurrencies = [
     body('cur_name').trim().optional()
-        .isLength({ min: 4, max: 255 }).withMessage('El nombre debe tener entre 4 y 255 caracteres.')
+        .isLength({ min: 3, max: 255 }).withMessage('El nombre debe tener entre 4 y 255 caracteres.')
         .matches(/^[a-zA-ZÁÉÍÓÚÑ\s]+$/).withMessage('El nombre solo puede contener letras.'),
     async (req, res) => {
-=======
-}
-const updateCurrencies = [
-    body('cur_name').optional().trim()
-        .isLength({ min: 4, max: 255 }).withMessage('El nombre debe tener entre 4 y 255 caracteres.')
-        .matches(/^[A-ZÁÉÍÓÚÑ\s]+$/).withMessage('El nombre solo puede contener letras en mayúsculas.'),
-    async (req, res) => {
-        // Validar los errores de los campos
->>>>>>> a76099568d18ec0cba8091bccf9e518223dac28e
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             const allErrors = errors.array().map(error => error.msg);
             return res.status(400).json({ errors: allErrors });
         }
-<<<<<<< HEAD
-=======
-        if (Object.keys(req.body).length === 0) {
-            return res.status(400).json({ message: 'No hay datos para actualizar' });
-        }
->>>>>>> a76099568d18ec0cba8091bccf9e518223dac28e
         try {
             const currency = await Currencies.findByPk(req.params.id);
             if (!currency) {
@@ -79,12 +63,8 @@ const updateCurrencies = [
         }
     }
 ];
-<<<<<<< HEAD
 
 const deleteCurrencies = async (req, res) => {
-=======
-const deleteCurrencies = async (req ,res )   =>{
->>>>>>> a76099568d18ec0cba8091bccf9e518223dac28e
     try {
         const currency = await Currencies.findByPk(req.params.id);
         if (!currency) {
